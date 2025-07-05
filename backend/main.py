5b7967d2976db3c584e4280f348030a4a99e4bc5
Ubuntu <ubuntu@ip-172-31-29-184.ec2.internal>
2025-07-05 23:41:42 +0000

    update

--- backend/main.py
+++ backend/main.py
@@ -135,7 +135,7 @@ def serve_static_assets(filename):
 
 
 def main():
-    socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='127.0.0.1', port=8000, allow_unsafe_werkzeug=True)
 
 if __name__ == '__main__':
     main()
