8c6fb3490f55b999d3b6e67d19e3eec80ba4fe6f
Andrew Gross <andrew144@sbcglobal.net>
2025-07-08 13:30:45 -0400

    import fix

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import {resolve} from 'node:path';
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? 'https://trendwars.io/' : 'https://localhost:5173/',
@@ -11,9 +12,10 @@ export default defineConfig(({ mode }) => ({
     setupFiles: './vitest.setup.mjs',
   },
   resolve: {
-  alias: {
-    // /esm/icons/index.mjs exports the icons statically, so no separate chunks are created
-    "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    alias: {
+      // /esm/icons/index.mjs exports the icons statically, so no separate chunks are created
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+      "@/assets": resolve(__dirname, "./src/assets"),
+    }
   }
-}
 }));
